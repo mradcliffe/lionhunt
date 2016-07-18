9934abffc2f7c375bcd773867e90fed6993b44be
Matthew Radcliffe <mradcliffe@softpixel.com>
2016-07-18 13:08:08 -0400

    Initial commit of lionhunt

---
+++ README.md
@@ -0,0 +1,85 @@
+# lionhunt
+
+lionhunt is a Workfront to JIRA export tool written in node.js that allows to export all of a Workfront Team's tasks as JIRA issues.
+
+This includes
+
+* Tasks
+* Task updates (comments only)
+* Task documents as external URLs
+
+## Usage
+
+```bash
+lionhunt -u username -p password -t "Workfront Team" -P "Workfront Project" <domain> <project key>
+```
+
+This will output to a file `export.json`.
+
+### Arguments
+
+#### domain
+
+The Workfront on-demand *domain* e.g. `domain.attask-ondemand.com`
+
+#### team
+
+Team name as a string enclosed by double quotes for a team name containing spaces.
+
+### Options
+
+#### username
+
+The Workfront user name to log in as. This is required, and if not provided as an option, lionhunt will prompt for the user name.
+
+#### password
+
+The corresponding password for the user name above. This is required, and if not provided as an option, lionhunt will prompt for the password.
+
+#### project
+
+An optional Workfront project to restrict to.
+
+#### team
+
+A required Worfkront team
+
+#### mapping
+
+A JSON file with an array of custom field mapping objects e.g.
+
+```json
+[
+  {
+    "from": "Acceptance Criteria",
+    "to": "Acceptance Criteria",
+    "type": "com.atlassian.jira.plugin.system.customfieldtypes:textarea"
+  }
+]
+```
+
+#### created
+
+Only hunt for tasks created after the specified ISO date.
+
+#### open
+
+Only hunt for open tasks.
+
+#### verbose
+
+Use verbose output. This is useful when running interactively, but should be disabled when providing all options on the command line.
+
+
+## TODO
+
+* Add `output` option to output to a file and output to STDOUT by default if not set.
+* Add `interactive` option and prompt for user name and password there, and disable auto-prompt. Error out instead.
+
+* Support nested comments. This is a pain in the butt to do and increases execution time/explodes the number of requests needed.
+
+### Probably won't do
+
+* Add support for issue history/transitions from Workfront.
+* Sub-tasks or issues.
+* Downloading documents and re-attaching to JIRA. This is only supported for linking to Workfront.
---
+++ index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var _ = require("lodash");
+var util = require("util");
+var inquirer = require("inquirer");
+var program = require("commander");
+var lionhunt = require("./lib/lionhunt");
+var optionsParse = require("./lib/options");
+var optionsParser = new optionsParse();
+var options = {domain: null, team: null, project: null};
+var prompts = [];
+var domainValue, teamValue;
+
+program
+    .version("0.1.0")
+    .description("Fetch a project's tasks, comments, and documents from WorkFront.")
+    .option("-u, --username [username]", "Workfront user name e.g. email address.")
+    .option("-p, --password [password]", "Workfront password.")
+    .option("-P, --project [project]", "Workfront project name.")
+    .option("-t, --team [team]", "Workfront team name.")
+    .option("-m, --mapping [file]", "A mapping file containing an array of objects.")
+    .option("-O, --open", "Include only open tasks.")
+    .option("-c, --created [date]", "Only hunt for tasks created after a valid ISO date e.g. 2016-01-01")
+    .option("-v, --verbose", "Provide verbose output for debugging.")
+    .arguments("<domain> <projectKey>")
+    .action(function (domain, project) {
+        options.domain = domain;
+        options.projectKey = project;
+    })
+    .parse(process.argv);
+
+optionsParser.parse(program);
+_.assign(options, optionsParser.get());
+
+if (!options.username) {
+    prompts.push({
+        type: "input",
+        message: "Username",
+        name: "name"
+    });
+}
+
+if (!options.password) {
+    prompts.push({
+        type: "password",
+        message: "Password",
+        name: "pass"
+    });
+}
+
+// Prompt for options that were not provided for an interactive, but not
+// required experience.
+inquirer
+    .prompt(prompts)
+    .then(function (answers) {
+        if (answers.name) {
+            options.username = answers.name;
+        }
+        if (answers.pass) {
+            options.password = answers.pass;
+        }
+        return this;
+    })
+    .then(function () {
+        var hunter;
+        if (null === options.domain || !options.team.length || null === options.projectKey) {
+            if (options.verbose) {
+                console.trace("Incorrect arguments provided:");
+            } else {
+                console.error("Incorrect arguments provided.");
+            }
+            program.help();
+            return;
+        }
+
+        hunter = new lionhunt(options);
+        hunter.login(options.username, options.password)
+            .then(hunter.stalk)
+            .then(hunter.feast)
+            .then(function(data) {
+                if (options.verbose) {
+                    console.log("Success!");
+                }
+                return hunter.api.logout();
+            })
+            .catch(function (error) {
+                if (options.verbose) {
+                    if (error.stack) {
+                        console.error(error.stack);
+                    } else {
+                        console.trace("Failure: " + util.inspect(error));
+                    }
+                } else {
+                    console.error("Failure: " + util.inspect(error));
+                }
+                return hunter.api.logout();
+            });
+    });
